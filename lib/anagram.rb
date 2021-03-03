3e34eb7b18c3cf2087f89d25f5d2543c3bc6a4a8
RobertObi <robobi97@googlemail.com>
2021-03-02 18:21:19 -0600

    commit tree 9e4ebb981c19a9a9a7e6f4b0944bf0fad0f4c5b3

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :object_name
+
+    @@all = []
+
+    def initialize(object_name)
+        @object_name = object_name
+        @@all << self
+    end
+
+    def match
+
+        empty_array = []
+        self.all.match
+       empty_array << self.all.match
+       binding.pry
+       0
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
+end
+
+
+#should detect no matches (FAILED - 1)
+#diaper = Anagram.new('diaper')
+
